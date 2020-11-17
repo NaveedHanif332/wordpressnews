580389fd63399339734e7b411f02ddd4cfdc2d28
Muhammad Naveed <naveed32001@gmail.com>
2020-11-17 14:05:18 +0500

    Initial commit

---
+++ app/src/main/java/com/itechnotion/wpnews/Home/ItemClickListener.java
@@ -0,0 +1,5 @@
+package com.itechnotion.wpnews.Home;
+
+public interface ItemClickListener<T> {
+    void onItemClicked(int position, SubCategoriesModel model);
+}
---
+++ app/src/main/java/com/itechnotion/wpnews/utils/SharedObjects.java
@@ -0,0 +1,125 @@
+package com.itechnotion.wpnews.utils;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.os.StrictMode;
+import android.preference.PreferenceManager;
+import android.support.multidex.MultiDexApplication;
+import android.support.v7.app.AlertDialog;
+import android.util.Patterns;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
+import com.facebook.stetho.Stetho;
+import com.google.gson.Gson;
+import com.itechnotion.wpnews.R;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Sunil on 23-Oct-16.
+ */
+public class SharedObjects extends MultiDexApplication {
+
+    public static Context context;
+    public PreferencesEditor preferencesEditor = new PreferencesEditor();
+    SharedPreferences.Editor editor;
+    SharedPreferences sharedPreference;
+    public static String PREF_NAME = "WpnewsPref";
+    public static Context getContext() {
+        return context;
+    }
+
+    public SharedObjects(Context context) {
+        this.context = context;sharedPreference = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        editor = sharedPreference.edit();
+
+        initializeStetho();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public void initializeStetho() {
+        Stetho.InitializerBuilder initializerBuilder = Stetho.newInitializerBuilder(context);
+        initializerBuilder.enableWebKitInspector(Stetho.defaultInspectorModulesProvider(context));
+        initializerBuilder.enableDumpapp(Stetho.defaultDumperPluginsProvider(context));
+        Stetho.Initializer initializer = initializerBuilder.build();
+        Stetho.initialize(initializer);
+    }
+
+    public void setColor(String header_color, String footer_color, String primary_color) {
+
+        editor.putString(AppConstants.KEY_HEADERCOLOR,header_color);
+        editor.putString(AppConstants.KEY_FOOTERCOLOR,footer_color);
+        editor.putString(AppConstants.KEY_PRIMARYCOLOR,primary_color);
+        editor.commit();
+        editor.apply();
+    }
+    public void setLogo(String logo) {
+
+        editor.putString(AppConstants.KEY_LOGO,logo);
+    }
+    public String getLogo() {
+        return   sharedPreference.getString(AppConstants.KEY_LOGO, String.valueOf(R.drawable.splash));//#FFFFFF
+
+    }
+    public String getHeaderColor() {
+        return   sharedPreference.getString(AppConstants.KEY_HEADERCOLOR,"#FFFFFF");//#FFFFFF
+
+    }
+    public String getFooterColor() {
+        return sharedPreference.getString(AppConstants.KEY_FOOTERCOLOR,"#98C9FF");//#FFFFFF
+    }
+    public String getPrimaryColor() {
+        return  sharedPreference.getString(AppConstants.KEY_PRIMARYCOLOR,"#007aff");//#007aff
+    }
+
+    public class PreferencesEditor {
+
+        public void setPreference(String key, String value) {
+            SharedPreferences sharedPreference = PreferenceManager.getDefaultSharedPreferences(context);
+            SharedPreferences.Editor editor = sharedPreference.edit();
+            editor.putString(key, value);
+            editor.apply();
+            editor.commit();
+        }
+
+        public String getPreference(String key) {
+            try {
+                SharedPreferences sharedPreference = PreferenceManager.getDefaultSharedPreferences(context);
+                return sharedPreference.getString(key, "");
+            } catch (Exception exception) {
+                return "";
+            }
+        }
+
+
+
+        public void setBoolean(String key, boolean value) {
+            SharedPreferences sharedPreference = PreferenceManager.getDefaultSharedPreferences(context);
+            SharedPreferences.Editor editor = sharedPreference.edit();
+            editor.putBoolean(key, value);
+            editor.commit();
+        }
+
+        public Boolean getBoolean(String key) {
+            try {
+                SharedPreferences sharedPreference = PreferenceManager.getDefaultSharedPreferences(context);
+                return sharedPreference.getBoolean(key, true);
+            } catch (Exception exception) {
+                return false;
+            }
+        }
+
+    }
+}
